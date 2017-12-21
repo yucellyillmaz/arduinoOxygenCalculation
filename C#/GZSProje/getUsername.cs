050e835c3db3d55db6f3512dd8018d613fa7805a
yclylmz <33909632+yclylmz@users.noreply.github.com>
2017-12-21 18:42:02 +0300

    Add files via upload

---
+++ C#/GZSProje/getUsername.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZSProje
+{
+    static class getUsername
+    {
+        public static int userID { get; set; }
+        public static string userName { get; set; }
+    }
+}
